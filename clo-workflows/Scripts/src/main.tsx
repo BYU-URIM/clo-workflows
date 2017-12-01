b9efc2d280d01afe14204409826284f6501e18ca
Connor Moody <cdmoody0604@gmail.com>
2017-12-01 13:33:37 -0700

    added env plugin and env code, added exceptions to tslint

--- clo-workflows/Scripts/src/main.tsx
+++ clo-workflows/Scripts/src/main.tsx
@@ -3,6 +3,7 @@ import * as ReactDom from 'react-dom'
 import { useStrict } from 'mobx'
 import { BrowserRouter, Router } from 'react-router-dom'
 import { App } from './layout/App'
+import { ENVIRONMENT, Environment } from './env';
 
 // in strict mode, mobx requires that all observable data members only be modified through explicit @action mutators
 useStrict(true)
