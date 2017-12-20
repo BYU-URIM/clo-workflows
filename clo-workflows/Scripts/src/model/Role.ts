bbd450844eafed11a48c086a04a67ca36052fdd0
Connor Moody <cdmoody0604@gmail.com>
2017-12-19 21:05:29 -0700

    import fix

--- clo-workflows/Scripts/src/model/Role.ts
+++ clo-workflows/Scripts/src/model/Role.ts
@@ -1,9 +1,9 @@
-import { IStep } from "./IStep"
+import { IStep } from "./Step"
 
 type RoleName = "anonymous" | "Junior License Processor" | "Senior License Processor"
                     | "Supervisor" | "Admin Assistant" | "Administrator"
 
 export interface IRole {
     name: RoleName
-    steps: Array<IStep> // steps which are visible to members of each role
+    permittedSteps: Array<IStep> // steps which are visible to members of each role
 }
