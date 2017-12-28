9e46f9e8320ba1ffd340555e9096804b8caa56d3
tgilland95 <tylergilland@gmail.com>
2017-12-28 13:45:54 -0700

    added yarn.lock and .yarnclean to gitignore

--- clo-workflows/Scripts/src/dataAccess/SPDataAccess.ts
+++ clo-workflows/Scripts/src/dataAccess/SPDataAccess.ts
@@ -22,5 +22,4 @@ export class SPDataAccess implements IDataAccess {
                 role: null,
             }))
     }
-
 }
--- clo-workflows/Scripts/src/layout/App.tsx
+++ clo-workflows/Scripts/src/layout/App.tsx
@@ -5,6 +5,10 @@ import { Fabric } from "office-ui-fabric-react/lib/Fabric"
 import Greeting from "../component/Greeting"
 import RoleChecker from "../component/RoleChecker";
 import DevTools from 'mobx-react-devtools'
+import { initializeIcons } from '@uifabric/icons';
+
+// Register icons and pull the fonts from the default SharePoint cdn.
+initializeIcons();
 @inject("rootStore")
 @observer
 export class App extends React.Component<any, any> {
