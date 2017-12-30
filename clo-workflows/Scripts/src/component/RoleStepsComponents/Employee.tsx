f4d6759aea11749fd986acf34910b985b809ff81
tgilland95 <tylergilland@gmail.com>
2017-12-29 18:50:53 -0700

    Renamed component to employee, now it renders for all employees

---
+++ clo-workflows/Scripts/src/component/RoleStepsComponents/Employee.tsx
@@ -0,0 +1,26 @@
+import * as React from "react"
+import { IUser } from "../../model/User"
+interface IEmployeeProps {
+  currentUser: IUser
+}
+const styles = {
+  border: "solid 2px black",
+  margin: "25px",
+  padding: "10px",
+}
+export const Employee = (props: IEmployeeProps) => {
+  return (
+    <div>
+      {props.currentUser.role.permittedSteps.map(step => (
+        <div key={`${step.name}-${step.processFormControls.length}`} style={styles}>
+          <h2>{step.name}</h2>
+          <ul>
+            {step.processFormControls.map(formControl => (
+              <li key={`${step.name}-${formControl.displayName}`}>{formControl.displayName}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  )
+}
