5f2f4bb04cdd806b9d77c5e85f6e48233ae8f634
tgilland95 <tylergilland@gmail.com>
2017-12-29 18:52:04 -0700

    added conditional render based on isEmployee boolean, fixed imports

--- clo-workflows/Scripts/src/layout/App.tsx
+++ clo-workflows/Scripts/src/layout/App.tsx
@@ -2,29 +2,34 @@ import * as React from "react"
 import { inject, observer } from "mobx-react"
 import { UserStore } from "../store/UserStore"
 import { Fabric } from "office-ui-fabric-react/lib/Fabric"
-import { RoleRenderContainer } from "./RoleRenderContainer"
+import { Anonymous } from "../component/RoleStepsComponents/Anonymous"
 import DevTools from "mobx-react-devtools"
 import { initializeIcons } from "@uifabric/icons"
-import Header from "../component/Header";
+import Header from "../component/Header"
+import { Employee } from "../component/RoleStepsComponents/Employee"
 
 // Register icons and pull the fonts from the default SharePoint cdn.
 initializeIcons()
 @inject("rootStore")
 @observer
 export class App extends React.Component<any, any> {
-	componentWillMount() {
-		this.userStore = this.props.rootStore.userStore
-	}
+  componentWillMount() {
+    this.userStore = this.props.rootStore.userStore
+  }
 
-	private userStore: UserStore
+  private userStore: UserStore
 
-	render() {
-		return (
-			<Fabric>
-				<Header currentUser={this.userStore.currentUser} />
-				<RoleRenderContainer currentUser={this.userStore.currentUser} />
-				<DevTools />
-			</Fabric>
-		)
-	}
+  render() {
+    return (
+      <Fabric>
+        <Header currentUser={this.userStore.currentUser} />
+        {this.userStore.isEmployee ? (
+          <Employee currentUser={this.userStore.currentUser} />
+        ) : (
+          <Anonymous currentUser={this.userStore.currentUser} />
+        )}
+        <DevTools />
+      </Fabric>
+    )
+  }
 }
