6e03c4c50da004bdac6e5990257ff77e547cf156
ty <tylergilland@gmail.com>
2018-01-02 20:05:16 -0700

    removed unused article data

--- clo-workflows/Scripts/src/component/NewProject.tsx
+++ clo-workflows/Scripts/src/component/NewProject.tsx
@@ -7,28 +7,7 @@ interface INewProjectProps{
   currentUser: IUser
 }
 const workType: Array<WorkType> = ["Article", "Book", "Book Chapter", "Image", "Music", "Other", "Video", "Website"]
-const work: IArticleWork = {
-  type:"Article",
-  id: 12345,
-  course: "some course",
-  format: "print",
-  title: "A super lame article",
-  author:"george washington",
-  publisher: "Random House",
-  yearPublished: "1999",
-  journalTitle: "a boring journal",
-  edition: "3rd",
-  formatInformation: "nothing special",
-  callNumber: "1355476",
-  ISSN: 123456789,
-  volume: 3,
-  issue: "some issue",
-  pageRange: "100-200",
-  totalPages: 1000,
-  URL: "www.url.com",
-  hardCopyOwner: "Mr. Obama",
- 
-}
+
 export class NewProject extends React.Component<INewProjectProps> {
   render(){
     return(
