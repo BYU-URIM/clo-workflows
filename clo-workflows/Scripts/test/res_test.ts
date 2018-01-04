85d95ea474dab0ef521a91052082b66b54c15cbb
Connor Moody <cdmoody0604@gmail.com>
2018-01-04 12:38:32 -0600

    added res tests and fixed JSONs

--- clo-workflows/Scripts/test/res_test.ts
+++ clo-workflows/Scripts/test/res_test.ts
@@ -1,7 +1,11 @@
 import * as ava from "ava"
 import * as roles from "../res/json/Roles.json"
 import * as steps from "../res/json/Steps.json"
-import * as processFormControls from "../../res/json/ProcessFormControls.json"
+import * as processFormControls from "../res/json/ProcessFormControls.json"
+import * as workFormControls from "../res/json/WorkFormControls.json"
+import * as projectFormControls from "../res/json/ProjectFormControls.json"
+import * as workTypes from "../res/json/WorkTypes.json"
+import * as projectTypes from "../res/json/ProjectTypes.json"
 import { IFormControl } from "../src/model/FormControl"
 
 /*
@@ -61,5 +65,77 @@ ava.test("test processFormControls for correct shape", t => {
         if(formControl.type === "choice") {
             t.truthy(formControl.choices)
         }
+    }
+})
+
+/*
+    ensure all JSON WorkFormContols have the correct shape:
+    {
+        displayName: string
+        modelRef: string
+        type: FormControlType
+        choices?: Array<string>
+    }
+*/
+ava.test("test workFormControls for correct shape", t => {
+    const formControlTypes = ["text", "choice", "checkbox", "textarea", "datetime", "number"]
+
+    for(const formControlName in workFormControls) {
+        const formControl: IFormControl= workFormControls[formControlName]
+        t.truthy(formControl.displayName)
+        t.truthy(formControl.modelRef)
+        t.truthy(formControl.type)
+        t.true(formControlTypes.indexOf(formControl.type) !== -1)
+        if(formControl.type === "choice") {
+            t.truthy(formControl.choices)
+        }
+    }
+})
+
+/*
+    ensure all JSON ProjectFormControls have the correct shape:
+    {
+        displayName: string
+        modelRef: string
+        type: FormControlType
+        choices?: Array<string>
+    }
+*/
+ava.test("test projectFormControls for correct shape", t => {
+    const formControlTypes = ["text", "choice", "checkbox", "textarea", "datetime", "number"]
+
+    for(const formControlName in projectFormControls) {
+        const formControl: IFormControl= projectFormControls[formControlName]
+        t.truthy(formControl.displayName)
+        t.truthy(formControl.modelRef)
+        t.truthy(formControl.type)
+        t.true(formControlTypes.indexOf(formControl.type) !== -1)
+        if(formControl.type === "choice") {
+            t.truthy(formControl.choices)
+        }
+    }
+})
+
+/*
+    ensure that all projectTypes contain references to exisiting form controls
+*/
+ava.test("test that project types contain only valid projectFormControl names", t => {
+    for(const projectType in projectTypes) {
+        const formControlNames: string[] = projectTypes[projectType]
+        formControlNames.forEach(formControlName => {
+            t.truthy(projectFormControls[formControlName])
+        })
+    }
+})
+
+/*
+    ensure that all workTypes contain references to exisiting form controls
+*/
+ava.test("test that work types contain only valid workFormControl names", t => {
+    for(const workType in workTypes) {
+        const formControlNames: string[] = workTypes[workType]
+        formControlNames.forEach(formControlName => {
+            t.truthy(workFormControls[formControlName])
+        })
     }
 })
