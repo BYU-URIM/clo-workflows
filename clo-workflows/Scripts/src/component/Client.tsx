4a94db346dbc5ae66990bad86c6a14dbcf78e201
tgilland95 <tylergilland@gmail.com>
2018-01-26 22:52:27 -0700

    switched to a choicegroup over a dropdown

--- clo-workflows/Scripts/src/component/Client.tsx
+++ clo-workflows/Scripts/src/component/Client.tsx
@@ -8,11 +8,15 @@ import { ClientStore } from "../store/ClientStore"
 import { Dropdown } from "office-ui-fabric-react/lib/Dropdown"
 import { SearchBox } from "office-ui-fabric-react/lib/SearchBox"
 import { Checkbox } from "office-ui-fabric-react/lib/Checkbox"
-import { ClientWorkType } from "./ClientWorkType"
+// import { ClientWorkType } from "./ClientWorkType"
 import { ClientProjectType } from "./ClientProjectType"
 import { Label } from "office-ui-fabric-react/lib/Label"
 import { Pivot, PivotItem, PivotLinkSize, PivotLinkFormat } from "office-ui-fabric-react/lib/Pivot"
-export interface IWorkTypeDropdownProps {
+import { ChoiceGroup, IChoiceGroupOption } from "office-ui-fabric-react/lib/ChoiceGroup"
+import { Toggle } from "office-ui-fabric-react/lib/Toggle"
+import {SubForm} from "./SubForm"
+import { PrimaryButton } from "office-ui-fabric-react/lib/Button"
+ export interface IWorkTypeDropdownProps {
   workTypes: Array<string>
   setNewProjectState: any
 }
@@ -24,6 +28,10 @@ const styles = {
   item: {
     margin: "10px 0px",
   },
+  content:{
+    margin: "15px 0px",
+    
+  }
 }
 
 @inject("rootStore")
@@ -35,21 +43,72 @@ export class Client extends React.Component<any, any> {
   }
   sessionStore: SessionStore
   clientStore: ClientStore
-
+  
   render() {
     const options = [{ key: "Header", text: "Work Types", itemType: Header }]
     const { sessionStore, clientStore } = this
-    const { newProject, newProjectState, DataService } = clientStore
+    const { newProject, viewState} = clientStore
+    const ops = [
+      {
+        key: "new",
+        text: "New Project",
+      },
+      {
+        key: "existing",
+        text: "Exisitng Project",
+      },
+    ]
+
     return (
       <div style={styles.main}>
         <Pivot linkSize={PivotLinkSize.large} linkFormat={PivotLinkFormat.tabs}>
           <PivotItem linkText="New Request">
-            <Label>Welcome {sessionStore.currentUser.name.split(" ")[0]}!</Label>
-            <ClientProjectType clientStore={this.clientStore} styles={styles} />
-            <ClientWorkType clientStore={this.clientStore} styles={styles} />
+            <div style={styles.content}>
+            {
+              !clientStore.getViewState.newRequestVisible ? 
+            <PrimaryButton              
+              text="Start New Request"
+              primary
+              onClick={()=> clientStore.updateMember("startNewRequest")}
+               />
+               : 
+               <PrimaryButton              
+               text="Cancel Request"
+               primary
+               onClick={()=> clientStore.clear()}
+                />
+            }
+            {clientStore.startNewRequest && 
+            <ChoiceGroup
+            selectedKey={clientStore.newOrExisting}
+            options={ops}
+            onChange={(event, selected) => clientStore.updateMember("newOrExisting", selected.key)}
+            label="Will your request be a part of a new or existing project?"
+            />
+            }
+            {
+              clientStore.getViewState.newOrExisting === "new" && 
+              <div>
+                <ClientProjectType clientStore = {this.clientStore} styles={styles} />
+              </div>
+            }
+            {
+              clientStore.getViewState.newOrExisting === "existing" &&
+              <SearchBox onFocus={() => console.log("onFocus called")} onBlur={() => console.log("onBlur called")} style={styles.item} />
+            }
+            {
+              
+              clientStore.newOrExisting === "existing" &&
+               <div>
+                existing
+              </div>
+            }
+              </div>
           </PivotItem>
           <PivotItem linkText="Pending Requests">
             <Label>Pivot #2</Label>
+            <ClientProjectType clientStore={this.clientStore} styles={styles} />
+            {/* <ClientWorkType clientStore={this.clientStore} styles={styles} /> */}
           </PivotItem>
           <PivotItem linkText="Completed Requests">
             <Label>Pivot #2</Label>
