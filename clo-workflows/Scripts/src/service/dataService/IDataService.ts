882f81da1ff27a99e04f33334b14beddb68f69f2
tgilland95 <tylergilland@gmail.com>
2018-02-21 21:24:34 -0700

    added fetchclientactiveprocesses

--- clo-workflows/Scripts/src/service/dataService/IDataService.ts
+++ clo-workflows/Scripts/src/service/dataService/IDataService.ts
@@ -7,6 +7,7 @@ export interface IDataService {
     fetchClientCompletedProjects(): Promise<Array<ICloRequestElement>>
     fetchEmployeeActiveProcesses(employee: IUser): Promise<Array<ICloRequestElement>>
     fetchEmployeeActiveProjects(employee: IUser): Promise<Array<ICloRequestElement>>
+    fetchClientActiveProcesses(client: IUser): Promise<Array<ICloRequestElement>>
     fetchEmployeeActiveWorks(employee: IUser): Promise<Array<ICloRequestElement>>
     fetchClientActiveProjects(client: IUser): Promise<Array<ICloRequestElement>>
 }
