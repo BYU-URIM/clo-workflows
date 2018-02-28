275f3776045d65e48149a6796e97a07d1cb96016
tgilland95 <tylergilland@gmail.com>
2018-02-27 22:00:35 -0700

    working with sp backend

--- clo-workflows/Scripts/src/component/ProjectProcessList.tsx
+++ clo-workflows/Scripts/src/component/ProjectProcessList.tsx
@@ -11,6 +11,7 @@ import { ClientStore } from "../store/ClientStore"
  * add whichever fields should be displayed like status and details
  * clean up 'extras' in component
  * actions and state of selections
+ * `
  *******************************/
 
 export interface IColumns {
@@ -21,79 +22,66 @@ export interface IColumns {
     maxWidth: number
     isResizable: boolean
 }
+export interface IProjectGroup extends IGroup {
+    submitterId: string
+}
 export interface IColumnsArray {}
 
 @inject("rootStore")
 @observer
 export class ProjectProcessList extends React.Component<any, any> {
-    /* Not currently used... */
     private selection: Selection
     clientStore: ClientStore
-    _processes: Array<{ projectId: any; processes?: Array<ICloRequestElement>; key: number; process: string; projectTitle: string }>
-    projects: Array<ICloRequestElement>
+    // processes are items in details list
+    _processes: any
     _columns: Array<IColumns>
-    _groups: IGroup[]
+    // projects are groups in details list
+    _projects: IProjectGroup[]
     constructor(props: {}) {
         super(props)
         this._processes = []
         this.clientStore = this.props.rootStore.clientStore
-        this._groups = this.clientStore.projects
-            .map((proj: ICloRequestElement): IGroup => ({
-                key: proj.id.toString(),
-                name: proj.title.toString(),
+        this._projects = this.clientStore.projects
+            .map((proj: ICloRequestElement): IProjectGroup => ({
+                key: proj.Id.toString(),
+                name: proj.Title.toString(),
                 count: this.clientStore.processes.filter(proc => {
-                    return proj.id === proc.projectId
+                    return proj.Id === Number(proc.projectId)
                 }).length,
+                submitterId: proj.submitterId.toString(),
                 startIndex: 0,
             }))
             .map((e, i, a) => {
                 i > 0 ? (e.startIndex = a[i - 1].count + a[i - 1].startIndex) : (e.startIndex = 0)
                 return e
             })
-
-        this.clientStore.processes.map((_proj) => {
+        this.clientStore.processes.map(_proj => {
             this._processes.push({
-                key: Number(_proj.id),
-                projectId: _proj.projectId,
-                process: _proj.step.toString(),
-                processes: [],
-                projectTitle: this.clientStore.projects[Number(_proj.projectId)].title.toString(),
+                ..._proj,
             })
         })
-        this._columns = [
-            {
-                key: "project",
-                name: "Project",
-                fieldName: "projectId",
-                minWidth: 100,
-                maxWidth: 200,
-                isResizable: true,
-            },
-            {
-                key: "process",
-                name: "Process",
-                fieldName: "step",
-                minWidth: 100,
-                maxWidth: 200,
-                isResizable: true,
-            },
-            {
-                key: "date",
-                name: "Date",
-                fieldName: "dateSubmittedToCurrentStep",
-                minWidth: 100,
-                maxWidth: 200,
-                isResizable: true,
-            },
-        ]
-        console.log(this)
+        const fields = ["Id", "Title", "payment", "processPayment", "requestSubmitted", "step", "workId", "submitterId"]
+        this._columns = fields.map((f, i): IColumns => ({
+            key: f,
+            name: f.split(/(?=[A-Z])/).join(" "),
+            fieldName: f,
+            minWidth: 75,
+            maxWidth: 300,
+            isResizable: true,
+        }))
     }
 
     public render() {
         return (
             <div>
-                <DefaultButton onClick={() => console.log(this._groups)} text="Add an item" />
-                <DetailsList items={this.clientStore.processes} groups={this._groups} columns={this._columns} />
+                <DefaultButton onClick={() => console.log(this)} text="Add an item" />
+                <DetailsList
+                    items={this._processes}
+                    compact={true}
+                    groups={this._projects}
+                    columns={this._columns}
+                    groupProps={{ showEmptyGroups: true }}
+                />
             </div>
         )
     }
