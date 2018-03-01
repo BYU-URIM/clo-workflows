6f06c5cddcd2dd20c4f7361c31ebb1dad64d2623
tgilland95 <tylergilland@gmail.com>
2018-03-01 11:40:37 -0700

    fetchEmployeeActiveObjects to match interface

--- clo-workflows/Scripts/src/service/dataService/SpDataService.ts
+++ clo-workflows/Scripts/src/service/dataService/SpDataService.ts
@@ -15,7 +15,12 @@ import { debug } from "util"
 
 // abstraction used to acess the SharePoint REST API
 // should only be used when the app is deployed against a SharePoint Instance conforming to the schema defined in "res/json/DB_CONFIG.json"
+
+window["pnp"] = pnp
 export class SpDataService implements IDataService {
+    fetchEmployeeActiveWorks(employee: IUser): Promise<ICloRequestElement[]> {
+        throw new Error("Method not implemented.")
+    }
     constructor(appWebUrl: string, hostWebUrl: string) {
         this.APP_WEB_URL = appWebUrl
         this.HOST_WEB_URL = hostWebUrl
@@ -26,13 +31,15 @@ export class SpDataService implements IDataService {
     // IDataService interface implementation
     async fetchUser(): Promise<IUser> {
         const rawUser = await this.getAppWeb().currentUser.get()
-        const rawRoles: any[] = await this.getAppWeb().siteUsers.getById(rawUser.Id).groups.get()
-        
+        const rawRoles: any[] = await this.getAppWeb()
+            .siteUsers.getById(rawUser.Id)
+            .groups.get()
+
         // TODO support multiple roles instead of using only roleNames[0] ?
-        const rawRole: any = (rawRoles.length && rawRoles[0]) || "Anonymous"
+        // const rawRole: any = (rawRoles.length && rawRoles[2]) || { Title: "Anonymous"}
+        const rawRole: any = { Title: "Anonymous" }
 
         const userName = this.extractUsernameFromLoginName(rawUser.LoginName)
-
         // build user object from userDto and role
         return {
             name: rawUser.Title,
@@ -41,7 +48,10 @@ export class SpDataService implements IDataService {
             role: getRole(rawRole.Title),
         }
     }
-
+    async fetchCurrentUserId(){
+        let currentUserId = await this.getAppWeb().currentUser.get()
+        return currentUserId.UserId.NameId
+    }
 
     // TODO add filter string to query for smaller requests and filtering on the backend
     async fetchEmployeeActiveProcesses(employee: IUser): Promise<Array<ICloRequestElement>> {
@@ -59,11 +69,11 @@ export class SpDataService implements IDataService {
     async fetchRequestElementsById(ids: number[], listName: ListName): Promise<ICloRequestElement[]> {
         const projects: Array<ICloRequestElement> = []
         const batch = this.getHostWeb().createBatch()
-        for(const id of ids) {
+        for (const id of ids) {
             const project = await this.getHostWeb()
                 .lists.getByTitle(listName)
                 .items.getById(id)
-                /*.inBatch(batch)*/.get(this.cloRequestElementParser)
+                /*.inBatch(batch)*/ .get(this.cloRequestElementParser)
             projects.push(project)
         }
         // await batch.execute()
@@ -72,8 +82,8 @@ export class SpDataService implements IDataService {
 
     async createRequestElement(requestElement: ICloRequestElement, listName: ListName): Promise<ICloRequestElement> {
         const result = await this.getHostWeb()
-            .lists.getByTitle(listName).items
-            .add(requestElement)
+            .lists.getByTitle(listName)
+            .items.add(requestElement)
         return result.data
     }
 
@@ -106,26 +116,25 @@ export class SpDataService implements IDataService {
             .get(this.cloRequestElementParser)
     }
 
-    fetchClientCompletedProjects(): Promise<Array<ICloRequestElement>> {
+    async fetchClientCompletedProjects(): Promise<Array<ICloRequestElement>> {
         return Promise.resolve(null)
     }
 
-    
-    fetchClientProjects(): Promise<Array<ICloRequestElement>> {
-        return Promise.resolve(null)
+    async fetchClientProjects(): Promise<Array<ICloRequestElement>> {
+        let clientProjects: Array<ICloRequestElement> = await this.getHostWeb()
+            .lists.getByTitle(ListName.PROJECTS).items.get()        
+        return clientProjects
+
     }
-    async fetchClientActiveProcesses(client: IUser): Promise<Array<ICloRequestElement>> {
+    async fetchClientProcesses(client: IUser): Promise<Array<ICloRequestElement>> {
         const activeProcesses: Array<ICloRequestElement> = await this.getHostWeb()
-            .lists.getByTitle(this.PROCESS_LIST_NAME)
-            .items.filter(this.ACTIVE_FILTER_STRING)
-            .get()
+            .lists.getByTitle(ListName.PROCESSES)
+            .items
+            .get(this.cloRequestElementParser)
 
         const permittedStepNames = client.role.permittedSteps.map(step => step.name)
-        return activeProcesses.filter(item => {
-            permittedStepNames.includes(item.step as string)
-        })
+        return activeProcesses
     }
-
 
     /******************************************************************************************************/
     // helper data and methods
@@ -133,7 +142,6 @@ export class SpDataService implements IDataService {
     private readonly HOST_WEB_URL: string
     private readonly APP_WEB_URL: string
     private readonly cloRequestElementParser: CloRequestElementParser
-    
 
     private getAppWeb(): Web {
         return pnp.sp.configure(
@@ -159,14 +167,15 @@ export class SpDataService implements IDataService {
 
     // the loginName string returned form the server contains some garbage appended to the username - take it out here
     private extractUsernameFromLoginName(loginName: string): string {
-        if(loginName.includes("\\")) {
+        if (loginName.includes("\\")) {
             return loginName.split("\\")[1]
-        } else if(loginName.includes("|")) {
+        } else if (loginName.includes("|")) {
             return loginName.split("|")[1]
         } else return ""
     }
-}
+    
 
+}
 
 // This custom parser filters out irrelevant fields and metadata from SharePoint REST responses to WORK, PROCESS, PROJECT, and NOTE queries
 // Only fields that are part of a CLO process, project, or request (as defined in res/DB_CONFIG.json) are preserved
@@ -182,8 +191,8 @@ export class CloRequestElementParser extends ODataDefaultParser {
         this.cloFieldSet.add(DB_CONFIG["defaultFields"])
     }
 
-     // set storing all fields specified in all tables of DB_CONFIG (as well as fields specified in DB_CONFIG.defaultFields)
-     // a set is used because it prevents duplicates (there are many field duplicates among the various tables), and because it provides fast lookup
+    // set storing all fields specified in all tables of DB_CONFIG (as well as fields specified in DB_CONFIG.defaultFields)
+    // a set is used because it prevents duplicates (there are many field duplicates among the various tables), and because it provides fast lookup
     private cloFieldSet: Set<string>
 
     // this method is called automatically by PNP once for each request
@@ -192,9 +201,9 @@ export class CloRequestElementParser extends ODataDefaultParser {
         const parsedResponse = await super.parse(response)
 
         // the parsedResponse may be an array of response objects or a single object, depending on what was requested
-        if(Array.isArray(parsedResponse)) {
+        if (Array.isArray(parsedResponse)) {
             return parsedResponse.map((singleResponseObject: {}) => this.filterSpResponseObject(singleResponseObject))
-        } else if(typeof parsedResponse === "object") {
+        } else if (typeof parsedResponse === "object") {
             return this.filterSpResponseObject(parsedResponse)
         } else {
             console.log("custom SpDataParser was unable to handle the data type of the response")
@@ -205,7 +214,7 @@ export class CloRequestElementParser extends ODataDefaultParser {
     // this method filters out all fields that are not defined in the cloFieldSet
     private filterSpResponseObject(spResponseObject: {}): {} {
         return Object.keys(spResponseObject).reduce((filteredResponseObject: {}, spReponseFieldName: string) => {
-            if(this.cloFieldSet.has(spReponseFieldName)) {
+            if (this.cloFieldSet.has(spReponseFieldName)) {
                 filteredResponseObject[spReponseFieldName] = spResponseObject[spReponseFieldName]
             }
             return filteredResponseObject
