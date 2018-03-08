d660c645554f6cf91049451887e29c05f6e13b69
tgilland95 <tylergilland@gmail.com>
2018-03-07 19:30:05 -0700

    added modals and command buttons

--- clo-workflows/Scripts/src/component/ProjectProcessList.tsx
+++ clo-workflows/Scripts/src/component/ProjectProcessList.tsx
@@ -1,26 +1,30 @@
-import { inject, observer } from "mobx-react"
+import { observer } from "mobx-react"
 import {
-    DefaultButton,
+    CommandBar,
     DetailsList,
     IGroup,
-    CommandBar,
-    SelectionMode,
-    IContextualMenuItem,
-    MarqueeSelection,
-    SelectionZone,
+    IColumn,
+    CheckboxVisibility,
+    IGroupDividerProps,
+    IRenderFunction,
+    DefaultButton,
+    IconType,
+    PrimaryButton,
+    CommandButton,
 } from "office-ui-fabric-react"
 import * as React from "react"
 
 import { CloRequestElement } from "../model/CloRequestElement"
 import { ClientStore } from "../store/ClientStore"
-import FormPanel from "./FormPanel"
+import ProjectFormModal from "./ProjectFormModal"
+import ProcessFormModal from "./ProcessFormModal"
+import WorkFormModal from "./WorkFormModal"
 
 /*******************************
  * TODO:
- * display title instead of project id
- * add whichever fields should be displayed like status and details
- * clean up 'extras' in component
- * actions and state of selections
+ * - view info for selected process
+ * - view vs process issues
+ * - form and general validation before submit
  *******************************/
 
 export interface IColumns {
@@ -40,12 +44,12 @@ export interface IProjectProcessListProps {
 
 @observer
 export class ProjectProcessList extends React.Component<IProjectProcessListProps, any> {
-    private selection: Selection
     clientStore: ClientStore
     /* processes for items in details list */
     _processes: Array<{}>
     _columns: Array<IColumns>
     _projects: IProjectGroup[]
+    _onRenderHeader
     constructor(props: IProjectProcessListProps) {
         super(props)
         this.clientStore = this.props.clientStore
@@ -53,12 +57,16 @@ export class ProjectProcessList extends React.Component<IProjectProcessListProps
         this._projects = this.clientStore.projects
             .map((proj: CloRequestElement, i): IProjectGroup => ({
                 key: i.toString(),
+                data: {
+                    projectId: proj.Id,
+                },
                 name: proj.Title.toString(),
                 count: this.clientStore.processes.filter(proc => {
                     return proj.Id === Number(proc.projectId)
                 }).length,
                 submitterId: proj.submitterId.toString(),
                 startIndex: 0,
+                isShowingAll: false,
             }))
             .map((e, i, a) => {
                 i > 0 ? (e.startIndex = a[i - 1].count + a[i - 1].startIndex) : (e.startIndex = 0)
@@ -71,60 +79,90 @@ export class ProjectProcessList extends React.Component<IProjectProcessListProps
                 projectId: proc.projectId,
                 submitterId: proc.submitterId,
                 Title: proc.Title,
-                ...proc,
             })
         })
-        const fields = ["Id", "Title", "workId", "submitterId"]
+        const fields = ["Title", "workId", "submitterId"]
         this._columns = fields.map((f, i): IColumns => ({
             key: i.toString(),
             name: f.split(/(?=[A-Z])/).join(" "),
             fieldName: f,
             minWidth: 75,
             maxWidth: 300,
-            isResizable: true,
+            isResizable: false,
         }))
+
+        this._onRenderHeader = (renderHeaderProps: IGroupDividerProps): JSX.Element => {
+            return (
+                <div>
+                    <span style={{ fontSize: "1.7em", marginRight: "10px" }}>
+                        <strong>Project Name:</strong> {renderHeaderProps.group!.name}
+                    </span>
+
+                    <CommandButton
+                        iconProps={{
+                            iconName: "Add",
+                        }}
+                        text="Add a Process"
+                        key={renderHeaderProps.group.key}
+                        onClick={e => {
+                            this.clientStore.updateNewProcess("projectId", renderHeaderProps.group.data.projectId)
+                            this.clientStore.updateViewState("showProcessModal", true)
+                        }}
+                    />
+                </div>
+            )
+        }
     }
     public render() {
-        const addNewProject = () => {}
         return (
             <div>
-                <FormPanel
+                <ProjectFormModal
                     clientStore={this.clientStore}
                     togglePanel={(m, v?) => {
                         this.clientStore.updateViewState(m, v)
                     }}
                 />
+                <ProcessFormModal
+                    clientStore={this.clientStore}
+                    togglePanel={(m, v?) => {
+                        this.clientStore.updateViewState(m, v)
+                    }}
+                />
+                <WorkFormModal
+                    clientStore={this.clientStore}
+                    togglePanel={(m, v?) => {
+                        this.clientStore.updateViewState(m, v)
+                    }}
+                />
+
                 <CommandBar
                     items={[
                         {
                             key: "addNewProject",
                             name: "Add New Project",
                             icon: "Add",
-                            onClick: () => this.clientStore.updateViewState("showProjectPanel", true),
+                            onClick: () => this.clientStore.updateViewState("showProjectModal", true),
                         },
                         {
-                            key: "addNewProcess",
-                            name: "Add Process to selected Project",
+                            key: "addNewWork",
+                            name: "Add New Work",
                             icon: "Add",
-                            onClick: e => {
-                                console.log(e.target)
-                            },
-                            disabled: !this.clientStore.isSelectedProject,
+                            onClick: () => this.clientStore.updateViewState("showWorkModal", true),
                         },
                     ]}
                 />
-                    <DetailsList
-                        items={this._processes}
-                        compact={true}
-                        
-                        groups={this._projects}
-                        columns={this._columns}
-                        groupProps={{
-                            showEmptyGroups: true,
-                        }}
-                        selection={this.clientStore.selectedProject}
-                        onActiveItemChanged={()=>this.clientStore.IsSelectedProject()}
-                    />
+                <DetailsList
+                    items={this._processes}
+                    groups={this._projects}
+                    columns={this._columns}
+                    checkboxVisibility={CheckboxVisibility.hidden}
+                    onRenderRow={(props, defaultRender) => <div onClick={e => console.log(e, props)}>{defaultRender(props)}</div>}
+                    groupProps={{
+                        showEmptyGroups: true,
+                        onRenderHeader: this._onRenderHeader,
+                    }}
+                    selection={this.clientStore.selectedProject}
+                />
             </div>
         )
     }
