8de825f36df77f9b25b11b704ebe6dcf4268da9a
Connor Moody <cdmoody0604@gmail.com>
2018-04-11 14:37:37 -0600

    add ProcessBriefList

--- clo-workflows/Scripts/src/component/EmployeeDashboard.tsx
+++ clo-workflows/Scripts/src/component/EmployeeDashboard.tsx
@@ -5,27 +5,7 @@ import { NonScrollableList } from "./NonScrollableList"
 import { RoleSteps } from "./RoleSteps"
 import { PrimaryButton } from "office-ui-fabric-react/lib/Button"
 import { SearchBox } from "office-ui-fabric-react"
-
-const wrapperStyle = {
-    margin: "80px 32px 80px 32px",
-    background: "#F8F8F8",
-    boxShadow: "0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)",
-    padding: "20px",
-    width: "500px"
-}
-const textStyle = {
-    font: "20px Segoe UI, sans-serif",
-    color: "#202020",
-    fontWeight: 200,
-    textAlign: "center",
-    marginTop: "20px"
-} as React.CSSProperties
-
-const processListTitleStyles = {
-    textAlign: "center",
-    font: "26px Segoe UI, sans-serif",
-    marginBottom: "8px",
-}
+import ProcessBriefList from "./ProcessBriefList"
 
 const searchProcessStyles = {
     maxWidth: "95%",
@@ -59,32 +39,22 @@ export class EmployeeDashboard extends React.Component<any, any> {
                         />
                     </div>
                 </div>
-                {employeeStore.isFocusSelectedStep && (
-                    <div style={wrapperStyle}>
-                        <div style={processListTitleStyles}>{`${employeeStore.selectedStep.name} Active Processes`}</div>
-                        {employeeStore.selectedStepProcessBriefs && employeeStore.selectedStepProcessBriefs.length ? (
-                            <NonScrollableList
-                                items={employeeStore.selectedStepProcessBriefs}
-                                onSelectItem={employeeStore.selectProcess}
-                            />
-                        ) : (
-                            <div style={textStyle}>No Active Processes</div>
-                        )}
-                    </div>
-                )}
-                {employeeStore.isFocusSearch && (
-                    <div style={wrapperStyle}>
-                        <div style={processListTitleStyles}>{`Process Search Results`}</div>
-                        {employeeStore.searchedProcessBriefs && employeeStore.searchedProcessBriefs.length ? (
-                            <NonScrollableList
-                                items={employeeStore.searchedProcessBriefs}
-                                onSelectItem={employeeStore.selectProcess}
-                            />
-                        ) : (
-                            <div style={textStyle}>no processes found</div>
-                        )}
-                    </div>
-                )}
+                {employeeStore.isFocusSelectedStep &&
+                    <ProcessBriefList
+                        processBriefs={employeeStore.selectedStepProcessBriefs}
+                        title={`${employeeStore.selectedStep.name} Active Processes`}
+                        emptyMessage="no active processes"
+                        onSelectProcessBrief={employeeStore.selectProcess}
+                    />
+                }
+                {employeeStore.isFocusSearch &&
+                    <ProcessBriefList
+                        processBriefs={employeeStore.searchedProcessBriefs}
+                        title="Search Results"
+                        emptyMessage="no results"
+                        onSelectProcessBrief={employeeStore.selectProcess}
+                    />
+                }
             </div>
         )
     }
---
+++ clo-workflows/Scripts/src/component/ProcessBriefList.tsx
@@ -0,0 +1,49 @@
+import * as React from "react"
+import { observer } from "mobx-react"
+import { IListItem, NonScrollableList } from "./NonScrollableList"
+
+const wrapperStyle = {
+    margin: "80px 32px 80px 32px",
+    background: "#F8F8F8",
+    boxShadow: "0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)",
+    padding: "20px",
+    width: "500px"
+}
+const textStyle = {
+    font: "20px Segoe UI, sans-serif",
+    color: "#202020",
+    fontWeight: 200,
+    textAlign: "center",
+    marginTop: "20px"
+} as React.CSSProperties
+
+const processListTitleStyles = {
+    textAlign: "center",
+    font: "26px Segoe UI, sans-serif",
+    marginBottom: "8px",
+}
+
+interface IProcessBriefListProps {
+    processBriefs: IListItem[]
+    title: string
+    emptyMessage: string
+    onSelectProcessBrief: (listItem: IListItem, itemIndex: number) => void
+}
+
+function ProcessBriefList(props: IProcessBriefListProps): JSX.Element {
+    return (
+        <div style={wrapperStyle}>
+            <div style={processListTitleStyles}>{`Process Search Results`}</div>
+            {props.processBriefs.length ? (
+                <NonScrollableList
+                    items={props.processBriefs}
+                    onSelectItem={props.onSelectProcessBrief}
+                />
+            ) : (
+                <div style={textStyle}>no processes found</div>
+            )}
+        </div>
+    )
+}
+
+export default observer(ProcessBriefList)
