affdbd8640a57f70fc0c8970debd9e86b141f714
tgilland95 <tylergilland@gmail.com>
2018-05-14 15:45:33 -0600

    these may happen later

--- clo-workflows/Scripts/webpack.parts.js
+++
@@ -1,9 +0,0 @@
-exports.devServer = ({ host, port } = {}) => ({
-    devServer: {
-        stats: "errors-only",
-        host, // Defaults to `localhost`
-        port, // Defaults to 8080
-        open: true,
-        overlay: true,
-    },
-})
